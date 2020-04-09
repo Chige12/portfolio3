16144d6509b6c6f72e14e9000bac0220415f3c0c
chige12 <tige1215.technic@gmail.com>
2020-04-09 17:51:42 +0900

    typescript 対応

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,8 @@
+const {
+  rules: bpRules
+} = require('eslint-config-airbnb-base/rules/best-practices')
+const { rules: styleRules } = require('eslint-config-airbnb-base/rules/style')
+
 module.exports = {
   root: true,
   env: {
@@ -14,10 +19,18 @@ module.exports = {
     'plugin:prettier/recommended',
     'plugin:nuxt/recommended'
   ],
-  plugins: [
-    'prettier'
-  ],
+  plugins: ['prettier'],
   // add your custom rules here
   rules: {
+    'vue/camelcase': styleRules.camelcase,
+    'vue/eqeqeq': bpRules.eqeqeq,
+    'vue/no-restricted-syntax': styleRules['no-restricted-syntax'],
+    'vue/component-name-in-template-casing': 0,
+    'vue/match-component-file-name': 0,
+    'vue/no-boolean-default': 0,
+    'vue/require-direct-export': 0,
+    'vue/v-on-function-call': 0,
+    'promise/always-return': 0,
+    'vue/no-parsing-error': 1
   }
 }
--- nuxt.config.js
+++ nuxt.config.js
@@ -65,13 +65,8 @@ export default {
      ** You can extend webpack config here
      */
     postcss: {
-      'postcss-cssnext': {
-        browsers: ['last 2 versions', 'ie >= 11', 'Android >= 4']
-      },
       preset: {
-        autoprefixer: {
-          grid: true
-        }
+        autoprefixer: { grid: 'autoplace' }
       }
     },
     extend(config, ctx) {
@@ -90,4 +85,4 @@ export default {
   }
 }
 
-// module.exports = nuxtConfig
+// export default config
